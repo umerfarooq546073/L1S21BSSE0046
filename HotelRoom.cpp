9530b68734154d750a3d49fa527f2e2f1c7100a7
umer farooq <umerfarooq546073@gmail.com>
2024-01-29 11:54:41 +0500

    hotelRoom

---
+++ HotelRoom.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+class HotelBookingSystem {
+private:
+    struct Room {
+        int roomNumber;
+        std::string roomType;
+        double price;
+        bool isAvailable;
+
+      
+    };
+
+}
+
+int main() {
+   
+    return 0;
+}
