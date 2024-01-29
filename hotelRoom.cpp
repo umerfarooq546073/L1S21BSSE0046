b264aa1b214022e81b2bf3b6a138fb1598164fa4
umerfarooq546073 <150656037+umerfarooq546073@users.noreply.github.com>
2024-01-29 12:30:11 +0500

    HotelRoom.cpp

---
+++ hotelRoom.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+class HotelBookingSystem {
+private:
+    struct Room {
+        int roomNumber;
+        std::string roomType;
+        double price;
+        bool isAvailable;
+public:
+void searchHotel
+{
+    cout<<"search Hotel";
+}
+      
+    };
+
+}
+
+int main() {
+   
+    return 0;
+}
